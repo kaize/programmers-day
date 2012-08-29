f6065e2167d747778b1059d7964c6c54fb8f3b12
Konstantin Kosmatov <key@kosmatov.su>
2012-08-29 18:17:20 +0400

    change staging deploy config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,5 +5,5 @@ role :db,  "62.76.184.52", :primary => true # This is where Rails migrations wil
 role :app, "62.76.184.52"
 role :web, "62.76.184.52"
 
-set :branch, "staging"
+set :branch, "develop"
 set :keep_releases, 3
