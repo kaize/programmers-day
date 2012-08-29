d593a8356d485da4ecc19095298f0efe7e281eda
Konstantin Kosmatov <key@kosmatov.su>
2012-08-29 19:02:06 +0400

    deploy environment

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ group :assets do
 end
 
 group :development do
+  gem 'capi'
   gem 'capistrano'
   gem 'capistrano_colors'
   gem 'capistrano-ext'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,36 @@
-set :rvm_type, :system
 set :stages, %w(production staging)
 set :default_stage, "staging"
-#set :whenever_command, "bundle exec whenever" # update crontab
+
+require 'capistrano/ext/multistage'
+require 'capi/unicorn'
+
+set :application, "programmers-day"
+set :rvm_type, :system
+
+set :scm, :git
+set :repository,  "git@github.com:kaize/programmers-day.git"
 
 set :use_sudo, false
 set :ssh_options, :forward_agent => true
 default_run_options[:pty] = true
 
-set :application, "programmers-day"
-
-set :scm, :git
-set :repository,  "https://github.com/kaize/programmers-day.git"
-
 namespace :deploy do
-  desc "Make symlink for database yaml"
-  task :symlink_db do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
     run "ln -nfs #{release_path}/config/database.yml.sample #{release_path}/config/database.yml"
   end
 end
 
-after "deploy:finalize_update", "deploy:symlink_db"
+namespace :capi do
+  desc 'invoke rake task. Example: cap capi:task_invoke TASK="db:seed"'
+  task :task_invoke  do
+    run "cd #{deploy_to}/current && bundle exec rake #{ENV['TASK']} RAILS_ENV=#{rails_env}"
+  end
+end
+
+before 'deploy:finalize_update', 'deploy:symlink_db'
 after "deploy:restart", "unicorn:stop"
 after "deploy:update", "deploy:cleanup"
 
+require 'capistrano_colors'
 require 'airbrake/capistrano'
