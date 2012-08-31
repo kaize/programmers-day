742861c3cba307f3d3dd471911d670bb42b4fba5
Konstantin Kosmatov <key@kosmatov.su>
2012-08-31 21:48:44 +0400

    change layouts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 set :user, 'programmers_day_production'
 set :rails_env, 'production'
 
-role :db,  "", :primary => true # This is where Rails migrations will run
-role :app, ""
-role :web, ""
+role :db,  "62.76.184.142", :primary => true # This is where Rails migrations will run
+role :app, "62.76.184.142"
+role :web, "62.76.184.142"
 
 set :branch do
   raise "Use tag. (Example: TAG=v2)" unless ENV['TAG']
