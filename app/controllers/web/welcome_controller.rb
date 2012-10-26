1ae873dd6cda421dbab30fa7ab201cd8002c28d7
Konstantin Kosmatov <key@kosmatov.su>
2012-10-26 16:21:43 +0400

    add subscribe page

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -2,6 +2,7 @@ class Web::WelcomeController < Web::ApplicationController
   def show
     @info = Page.find_by_uri('info')
     @feedback = Page.find_by_uri('feedback')
+    @subscribe = Page.find_by_uri('subscribe')
     @news = News.published.by_published_at.page
     @member = User.new process_personal_data: true, subscribe: true
   end
