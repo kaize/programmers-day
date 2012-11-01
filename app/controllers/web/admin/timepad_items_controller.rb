0bc920a967687e5ab1bb71b5b7b0fcb99647e0ee
Konstantin Kosmatov <key@kosmatov.su>
2012-11-01 16:01:30 +0400

    add notice for blank timepad maillist id

--- app/controllers/web/admin/timepad_items_controller.rb
+++ app/controllers/web/admin/timepad_items_controller.rb
@@ -1,9 +1,14 @@
 class Web::Admin::TimepadItemsController < Web::Admin::ApplicationController
   def export
-    maillist = TimepadMaillist.find params[:timepad_maillists][:id]
-    TimepadItemsExporter.run maillist.original_id
+    if params[:timepad_maillists][:id].present?
+      maillist = TimepadMaillist.find params[:timepad_maillists][:id]
+      TimepadItemsExporter.run maillist.original_id
 
-    flash_success
+      flash_success
+    else
+      flash_notice
+    end
+
     redirect_to :back
   end
 end
