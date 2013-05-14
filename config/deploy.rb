a68fd07c5466ea2d619806d148901bf219c50de3
Alexey Frank <alexeyfrank@gmail.com>
2013-05-14 19:46:04 +0400

    change git repo link

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, "prog73"
 set :rvm_type, :system
 
 set :scm, :git
-set :repository,  "git@github.com:kaize/programmers-day.git"
+set :repository,  "git://github.com/kaize/programmers-day.git"
 
 set :use_sudo, false
 set :ssh_options, :forward_agent => true
