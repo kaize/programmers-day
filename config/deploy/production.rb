9af896fdee5bc937143b12ebaee88332f5243ac0
Alexey Frank <alexeyfrank@gmail.com>
2013-05-14 19:29:21 +0400

    change servers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,15 @@
-set :user, 'programmers_day_production'
+set :user, 'prog73_production'
 set :rails_env, 'production'
 
-role :db,  "62.76.184.142", :primary => true # This is where Rails migrations will run
-role :app, "62.76.184.142"
-role :web, "62.76.184.142"
+role :db,  "62.76.191.134", :primary => true # This is where Rails migrations will run
+role :app, "62.76.191.134"
+role :web, "62.76.191.134"
 
 set :branch do
   raise "Use tag. (Example: TAG=v2)" unless ENV['TAG']
   ENV['TAG']
 end
-set :deploy_to, "/u/apps/programmers-day"
+set :deploy_to, "/u/apps/prog73"
 set :keep_releases, 15
 
 #set :whenever_command, "bundle exec whenever"
